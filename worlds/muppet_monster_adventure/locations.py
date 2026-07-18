3a04cfd98d520044c6b4e9725b2edaabdd1254e2
heatblayze <joshuajones2010@gmail.com>
2026-07-18 22:44:57 +1000

    more amulet locations and requirements

--- worlds/muppet_monster_adventure/locations.py
+++ worlds/muppet_monster_adventure/locations.py
@@ -7,6 +7,9 @@ class Ability(StrEnum):
     GLIDE = "glide"
     PUSH = "push"
     SMASH = "smash"
+    # TODO: implement these
+    GLOVE = "glove"
+    SPIN = "spin"
 
 
 class MMALocationData:
@@ -16,10 +19,18 @@ class MMALocationData:
         self.ability_requirements = ability_requirements
 
 
-# TODO: Improve names?
+# TODO: Improve names
 peacock_purgatory_amulet_locations: dict[str, MMALocationData] = {
     "Wocka Wocka Werebear Amulet - By tutorial flags": MMALocationData(),
-    "Wocka Wocka Werebear Amulet - On stairs near hedge trimmer": MMALocationData(),
+    "Wocka Wocka Werebear Amulet - On stairs near gardener": MMALocationData(),
     "Wocka Wocka Werebear Amulet - On hill by lake": MMALocationData(),
     "Wocka Wocka Werebear Amulet - By climbable wall": MMALocationData(),
+    "Muck Monster Amulet - By the lake": MMALocationData(),
+    "Muck Monster Amulet - Up climbable wall by Werebear Amulet": MMALocationData([Ability.CLIMB]),
+    "Muck Monster Amulet - On path before climable wall": MMALocationData(),
+    "Muck Monster Amulet - Up climable wall by Muck Monster Amulet": MMALocationData([Ability.CLIMB]),
+    "Noseferatu Amulet - Bottom of the lake": MMALocationData([Ability.SWIM]),
+    "Noseferatu Amulet - By sundial": MMALocationData(),
+    "Noseferatu Amulet - Up super-jump platform": MMALocationData(),
+    "Noseferatu Amulet - Up stairs after triggering switch": MMALocationData([Ability.GLOVE]),
 }
